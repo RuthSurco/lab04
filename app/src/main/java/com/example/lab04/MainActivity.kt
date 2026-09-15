ce5c544671d10ab566bd5004c035374bdb4091e2
Ruth <ruth.surco@tecsup.edu.pe>
2026-09-15 00:15:06 -0500

    Agregar componente Spacer

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -15,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -40,11 +44,22 @@ fun PantallaPrincipal() {
             modifier = Modifier.padding(innerPadding)
         ) {
 
-            Image(
-                painter = painterResource(id = R.drawable.img),
-                contentDescription = "Imagen de ejemplo",
-                modifier = Modifier.fillMaxWidth()
-            )
+            Column {
+
+                Image(
+                    painter = painterResource(id = R.drawable.img),
+                    contentDescription = "Imagen de ejemplo",
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(
+                    modifier = Modifier.height(40.dp)
+                )
+
+                Text(
+                    text = "Este espacio fue creado con Spacer"
+                )
+            }
         }
     }
 }
